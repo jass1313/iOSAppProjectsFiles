aeda52be1c822bf4344014197cc03e3cbfea4f86
Jasbeer Singh <38327762+jass1313@users.noreply.github.com>
2018-04-13 15:11:19 +0530

    new app files
    update

---
+++ ButtonCircles/i Am Rich/i Am Rich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  i Am Rich
+//
+//  Created by Jasbeer Singh on 03/04/18.
+//  Copyright © 2018 Jasbeer Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
